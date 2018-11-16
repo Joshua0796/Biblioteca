04395e7d6e463049cc5e06854d1eb569f3f14be2
Joshua Rodriguez <joshuaa.rodriguezr@hotmail.com>
2018-11-16 00:38:41 -0400

    Conection biblioteca

---
+++ Biblioteca/Conection/DbBiblioteca.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Conection
+{
+    public class dbBiblioteca : DbContext
+    {
+        //public DbSet<Usuarios> Usuarios {get; set;}
+        //public DbSet<Libros> Libros {get; set;}
+        //public DbSet<Autores> Autores {get; set;}
+    }
+}
